5ffd626b56078d38006f5b847311ff870022b27f
Kacper Reszka <kapiresz@gmail.com>
2023-01-12 21:18:18 +0100

    Add LoginPage and Error Message

---
+++ lib/HomePage/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'HomePage/home_page.dart';
+import 'LoginPage/login-page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,11 +10,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const RootPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
 
   // This widget is the root of your application.
   @override
@@ -22,13 +25,15 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Siema'),
-            ],
-          ),
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            final user = snapshot.data;
+            if (user == null) {
+              return LoginPage();
+            }
+            return const HomePage();
+          },
         ),
       ),
     );
