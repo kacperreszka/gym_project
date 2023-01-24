29ee758e1fe857dde7226a25762645bd303e8fec
Kacper Reszka <kapiresz@gmail.com>
2023-01-24 20:20:08 +0100

    Add CheckBoxListTile

--- lib/futures/training/traning_page_content.dart
+++ lib/futures/training/traning_page_content.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gym_project/futures/training/cubit/training_cubit.dart';
 
-class TrainingPageContent extends StatelessWidget {
+class TrainingPageContent extends StatefulWidget {
   const TrainingPageContent({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<TrainingPageContent> createState() => _TrainingPageContentState();
+}
+
+class _TrainingPageContentState extends State<TrainingPageContent> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +34,16 @@ class TrainingPageContent extends StatelessWidget {
               for (final document in documents) ...[
                 Text(document['name']),
               ],
+              CheckboxListTile(
+                activeColor: Colors.green,
+                controlAffinity: ListTileControlAffinity.leading,
+                title: const Text('Siema'),
+                subtitle: const Text('Co dziś robię?'),
+                value: isChecked,
+                onChanged: ((value) {
+                  setState(() => isChecked = value!);
+                }),
+              ),
             ],
           );
         },
